b0a4dc5840bff8e3de7f3793e487764af7dee2b3
hunbp75 <hunbp75@gmail.com>
2022-02-08 12:41:20 +0100

    initial Commit

---
+++ uibuild-biz-cards/src/components/Landing.jsx
@@ -0,0 +1,28 @@
+const Landing = () => {
+  return (
+    <div className="container">
+      <div className="card1">
+        <h2 className="name">Name 1</h2>
+        <h6>Restaurant 1</h6>
+        <p>address</p>
+        <p>address Lorem Ipsum</p>
+      </div>
+
+      <div className="card2">
+        <h2 className="name">Name 2</h2>
+        <h6>Restaurant 2</h6>
+        <p>address 2</p>
+        <p>address Lorem Ipsum</p>
+      </div>
+
+      <div className="card3">
+        <h2 className="name">Name 3</h2>
+        <h6>Restaurant 3</h6>
+        <p>address 3</p>
+        <p>address Lorem Ipsum</p>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
